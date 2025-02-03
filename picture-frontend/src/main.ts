5f9a5f3be3681401986a6fa3e9e6d2cd781f22ae
cuicui <2849128038@qq.com>
2025-02-03 21:10:29 +0800

    AI 图片编辑 - 前端 1. 基础图片编辑（裁剪、旋转、缩放） 2. AI 图片编辑（AI扩图）

--- picture-frontend/src/main.ts
+++ picture-frontend/src/main.ts
@@ -6,8 +6,14 @@ import Antd from 'ant-design-vue';
 import App from './App.vue'
 import router from './router'
 import '@/access.ts'
+import VueCropper from 'vue-cropper';
+import 'vue-cropper/dist/index.css'
+
+
 
 const app = createApp(App)
+
+app.use(VueCropper)
 
 app.use(createPinia())
 app.use(router)
